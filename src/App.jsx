fa1b44dbfa54b836652ba541741d8caeeb3bb075
Zeyneeppp <96092717+Zeyneeppp@users.noreply.github.com>
2023-08-27 02:37:17 +0200

    controller

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,55 @@ import FormLabel from '@mui/material/FormLabel';
 import FormGroup from '@mui/material/FormGroup';
 import Checkbox from '@mui/material/Checkbox';
 import hij from "./img/hij.png";
-
+import FormHelperText from '@mui/material/FormHelperText';
+import { useState } from 'react';
 
 function App() {
 
+  //-------------------Text min 70 word hhh -------------
+  const [inputValue, setInputValue] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const minWordCount = 70;
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
+
+  const validateInput = () => {
+    if (!submitted) {
+      return true; // makach erreur avant la soumission
+    }
+    
+    const wordCount = inputValue.split(/\s+/).filter((word) => word !== '').length;
+    return wordCount >= minWordCount;
+  };
+
+  //---------------- Max 3 fields
+  const maxChecked = 3;
+  const [checkedCount, setCheckedCount] = useState(0);
+
+  const handleCheckboxChange = (event) => {
+    const isChecked = event.target.checked;
+    const newCount = isChecked ? checkedCount + 1 : checkedCount - 1;
+
+    if (newCount <= maxChecked) {
+      setCheckedCount(newCount);
+    }
+    // else => empêche la coche
+  };
+
+  //bool pour verifier user a coché plus de 3
+  const hasExceededMaxChecked = checkedCount > maxChecked;
+  //if yes (checker plus de 3 error = true)
+
+ 
+  
   return (
 
     <div className="App">
@@ -48,12 +93,30 @@ function App() {
       <FormControl>
        <FormLabel id="demo-checkbox-group-label">Field</FormLabel>
        <FormGroup aria-labelledby="demo-checkbox-group-label">
-          <FormControlLabel control={<Checkbox />} label="UI/UX Design"/>
-          <FormControlLabel control={<Checkbox />} label="Web Dev"/>
-          <FormControlLabel control={<Checkbox />} label="Git/Linux"/>
-          <FormControlLabel control={<Checkbox />}  label="Intro to AI" />
-          <FormControlLabel control={<Checkbox />} label="3D Modeling"  />
+       <FormControlLabel
+            control={<Checkbox onChange={handleCheckboxChange} />}
+            label="UI/UX Design"
+          />
+          <FormControlLabel
+            control={<Checkbox onChange={handleCheckboxChange} />}
+            label="Web Dev"
+          />
+          <FormControlLabel
+            control={<Checkbox onChange={handleCheckboxChange} />}
+            label="Git/Linux"
+          />
+          <FormControlLabel
+            control={<Checkbox onChange={handleCheckboxChange} />}
+            label="Intro to AI"
+          />
+          <FormControlLabel
+            control={<Checkbox onChange={handleCheckboxChange} />}
+            label="3D Modeling"
+          />
        </FormGroup>
+       {hasExceededMaxChecked && (
+          <FormHelperText>Maximum 3 Fields allowed.</FormHelperText>
+        )}
       </FormControl>
 
 
@@ -61,15 +124,21 @@ function App() {
           id="fullWidth"
           label="What did u got from the trainings"
           multiline
-          rows={5}
-        />
+          rows={4}
+          value={inputValue}
+          onChange={handleInputChange}
+          error={!validateInput()}
+          helperText={
+            !validateInput() ? `Minimum word count: ${minWordCount}` : ''
+          }
+      />
       </div>
 
 
 
     <div className='send-button'>
     <Stack direction="row" spacing={2}>
-      <Button variant="contained" endIcon={<SendIcon />}>
+      <Button type= "submit" variant="contained" endIcon={<SendIcon />}>
         Send
       </Button>
     </Stack>
